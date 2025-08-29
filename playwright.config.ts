3f5009a19584c144ed902bf6b1c9ce6cc7204415
Dmytro Yerchak <umkauskas@gmail.com>
2025-08-29 09:25:20 +0300

    headless mode switch to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.saucedemo.com/',
-    headless: false,
+    headless: true,
     ignoreHTTPSErrors: true,
     navigationTimeout: 20000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
